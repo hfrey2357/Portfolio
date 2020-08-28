195a992b87dfc9b3e315756405a0f13af1ac2c35
hfrey2357 <45570287+hfrey2357@users.noreply.github.com>
2020-08-28 17:22:31 -0500

    Add project files.

---
+++ PortfolioSite/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PortfolioSite.Models;
+
+namespace PortfolioSite.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
